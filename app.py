5a0496a72e4ef267e61f64bfc3ec62bac39ed31c
Gloria Villa <hello.gloria@gmail.com>
2024-04-09 23:51:23 -0700

    added logic to jinja template

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
+import random
 
 # Create Flask Instance
 app = Flask(__name__)
-
-# to hot load you can run on command line flask run --debug
 
 # Create a route decorator
 """ @app.route('/')
@@ -12,13 +11,18 @@ def index():
 
 @app.route('/')
 def index():
-    return render_template("index.html") #Flask knows that all templates are in template directory
+    weather = ["rainy","cloudy","misty"]
+    # Select a random item
+    random_weather = random.choice(weather)
+    #first_name = "john"
+    return render_template('index.html', random_weather=random_weather, weather=weather)
 
-
-
+    #return render_template("index.html") #Flask knows that all templates are in template directory
 
 #localhost:5000/user/John
 @app.route('/user/<name>')
-
 def user(name):
-    return "<h1>Hello {}</h1>".format(name)
+    #return "<h1>Hello {}</h1>".format(name)
+    return render_template("user.html", user_name=name)
+
+# to hot load you can run on command line flask run --debug
